e24885859f47178ff8d97386446fddb059175ce0
Abdullah Abbasi <ababbasi@ebay.com>
2025-07-19 19:54:18 -0700

    fix: update skills

--- src/components/sections/SkillsSection.js
+++ src/components/sections/SkillsSection.js
@@ -5,15 +5,15 @@ const SkillsSection = () => {
   const skillData = [
     {
       category: "Regulatory Submissions and Technical Writing",
-      details: "510(k), EU-MDR Technical documentation, GSPR, IFU, Labels, Clinical and post market surveillance, SOPs, Work Instructions, Change assessment, CAPA, Deviation, Risk management, Design control file and Process validation plan"
+      details: "EU MDR Technical documentation, GSPR, IFU and labels, Clinical and post market surveillance, SOPs, Work Instructions, Change assessment, CAPA, Deviation, Risk management, Design control file and Process validation plan"
     },
     {
       category: "Regulatory Frameworks & Quality Standards",
-      details: "EU-MDR, US FDA guidance documents, ISO 13485, 21CFR 820, 21CFR Part 11, ISO 14971, ISO 15223-1, ISO 20417, EU-IVDR, IMDRF, and other ISO/ IEC standards"
+      details: "EU MDR 2017/745, US FDA guidance documents, ISO 13485, 21CFR 820, 21CFR Part 11, ISO 14971, ISO 15223-1, ISO 20417, EU-IVDR, IMDRF, and other ISO/ IEC standards"
     },
     {
       category: "Tools & Databases",
-      details: "MS Office suite, Adobe Acrobat, Adobe express, Canva, Notion, Veeva, Smartsheet, SharePoint, SAP, Virje, JIRA, BSI Compliance Navigator, GUDID, EUDAMED, FDA database, e Star Forms, Nyquist AI, IQVIA database, Canva, e Lab notebooks, MDCG"
+      details: "MS Office suite, Adobe Acrobat, Adobe express, Canva, Notion, Veeva, Smartsheet, SharePoint, SAP, Virje, JIRA, BSI Compliance Navigator, GUDID, EUDAMED, FDA database, e Star Forms, Nyquist AI, IQVIA database, e Lab notebooks, MDCG"
     },
     {
       category: "Languages",
